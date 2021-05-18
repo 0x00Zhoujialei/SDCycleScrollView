7290dac2fc8bfeb8839afd1508b61d1532a5b2dd
0x00Zhoujialei <dylzjl@gmail.com>
2021-05-18 17:46:50 +0800

    * fix

--- SDCycleScrollView/Lib/SDCycleScrollView/SDCycleScrollView.m
+++ SDCycleScrollView/Lib/SDCycleScrollView/SDCycleScrollView.m
@@ -371,7 +371,7 @@ NSString * const ID = @"SDCycleScrollViewCell";
 {
     [self invalidateTimer]; // 创建定时器前先停止定时器，不然会出现僵尸定时器，导致轮播频率错误
     
-    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:self.timerSecond target:self selector:@selector(automaticScroll) userInfo:nil repeats:YES];
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:self.timerSecond target:self selector:@selector(updateProgress) userInfo:nil repeats:YES];
     _timer = timer;
     [[NSRunLoop mainRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
 }
